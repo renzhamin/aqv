0aef1109eb6e0cc2add081c1fb496e0f34f56ce0
farabi <rafid.nahiyan.farabi.85@gmail.com>
2023-11-03 21:43:31 +0600

    cache added in socio-economic api

--- backend/src/data/helpers.js
+++ backend/src/data/helpers.js
@@ -9,13 +9,16 @@ const aqi_api =
   "https://api.weatherbit.io/v2.0/current/airquality?city={{city}}&key=" +
   apiKey;
 
-const gdp_api = "https://api.worldbank.org/v2/country/{{country}}/indicator/NY.GDP.MKTP.CD?date=2022&format=json";
+const gdp_api =
+  "https://api.worldbank.org/v2/country/{{country}}/indicator/NY.GDP.MKTP.CD?date=2022&format=json";
 const gdp_per_capita_api =
   "https://api.worldbank.org/v2/country/{{country}}/indicator/NY.GDP.PCAP.PP.CD?date=2022&format=json";
-const gdp_growth_api = "https://api.worldbank.org/v2/country/{{country}}/indicator/NY.GDP.MKTP.KD.ZG?date=2022&format=json";
-const population_api = "https://api.worldbank.org/v2/country/{{country}}/indicator/SP.POP.TOTL?date=2022&format=json";
-const population_growth_api = "https://api.worldbank.org/v2/country/{{country}}/indicator/SP.POP.GROW?date=2022&format=json";
-
+const gdp_growth_api =
+  "https://api.worldbank.org/v2/country/{{country}}/indicator/NY.GDP.MKTP.KD.ZG?date=2022&format=json";
+const population_api =
+  "https://api.worldbank.org/v2/country/{{country}}/indicator/SP.POP.TOTL?date=2022&format=json";
+const population_growth_api =
+  "https://api.worldbank.org/v2/country/{{country}}/indicator/SP.POP.GROW?date=2022&format=json";
 
 export async function get_city_info(cityname) {
   const city_info = cached_data.get(cityname);
@@ -35,36 +38,38 @@ export async function get_city_info(cityname) {
 }
 
 export async function get_country_info(country_code) {
-    const gdp = await fetchData(gdp_api, country_code);
-    const gdpPerCapita = await fetchData(gdp_per_capita_api, country_code);
-    const gdpGrowth = await fetchData(gdp_growth_api, country_code);
-    const population = await fetchData(population_api, country_code);
-    const populationGrowth = await fetchData(population_growth_api, country_code);
+  let countryInfo = cached_data.get(country_code);
+  if (countryInfo) return [countryInfo];
 
-    const countryInfo = {
-        gdp: getValue(gdp),
-        gdpPerCapita: getValue(gdpPerCapita),
-        gdpGrowth: getValue(gdpGrowth),
-        population: getValue(population),
-        populationGrowth: getValue(populationGrowth)
-    };
+  const gdp = await fetchData(gdp_api, country_code);
+  const gdpPerCapita = await fetchData(gdp_per_capita_api, country_code);
+  const gdpGrowth = await fetchData(gdp_growth_api, country_code);
+  const population = await fetchData(population_api, country_code);
+  const populationGrowth = await fetchData(population_growth_api, country_code);
 
-    console.log(countryInfo);
-    return [countryInfo];
+   countryInfo = {
+    gdp: getValue(gdp),
+    gdpPerCapita: getValue(gdpPerCapita),
+    gdpGrowth: getValue(gdpGrowth),
+    population: getValue(population),
+    populationGrowth: getValue(populationGrowth),
+  };
+
+  cached_data.set(country_code, countryInfo);
+
+  return [countryInfo];
 }
 
 async function fetchData(api, countryCode) {
-    const url = api.replace("{{country}}", countryCode);
-    const response = await fetch(url);
-    const data = await response.json();
-    return data[1][0].value;
+  const url = api.replace("{{country}}", countryCode);
+  const response = await fetch(url);
+  const data = await response.json();
+  return data[1][0].value;
 }
 
 function getValue(data) {
-    return data || 0;
+  return data || 0;
 }
-
-
 
 export async function get_cities_by_aqi(get_cleanest = true) {
   let sortOrder = "asc";
