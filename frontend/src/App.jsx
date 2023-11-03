ef44153578f07c8793cb844d22a82077aa1c7000
renzhamin <renzhamin@gmail.com>
2023-11-03 22:31:56 +0600

    move city data to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from "react"
-import { Button } from "./components/ui/button"
-import TopTen from "./pages/topTen/TopTen"
-import MapsComponent from "./pages/maps/MapsComponent"
-import AirQuality from "./pages/airQuality/AirQuality"
-import Navigation from "./pages/navigation/Navigation"
-import Footer from "./pages/footer/Footer"
-import HomePage from "./pages/homepage/HomePage"
+import { useEffect, useState } from "react";
+import HomePage from "./pages/homepage/HomePage";
+
+import { createContext } from "react";
+import { get_cities_by_aqi } from "./fetch/rankings";
+
+export const AppContext = createContext();
 
 function App() {
-    const [count, setCount] = useState(0)
+  const [worst, setWorst] = useState([]);
+  const [best, setBest] = useState([]);
+  useEffect(() => {
+    get_cities_by_aqi(false).then((data) => {
+      setWorst(data.slice(0, 10));
+    });
+    get_cities_by_aqi(true).then((data) => {
+      setBest(data.slice(0, 10));
+    });
+  }, []);
 
-    return (
-        <>
-            <HomePage/>
-            {/* <div>
+  return (
+    <>
+      <AppContext.Provider
+        value={{
+          worst,
+          best,
+        }}
+      >
+        <HomePage />
+        {/* <div>
                 <Button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </Button>
@@ -21,8 +35,9 @@ function App() {
                     Edit <code>src/App.jsx</code> and save to test HMR
                 </p>
             </div> */}
-        </>
-    )
+      </AppContext.Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
