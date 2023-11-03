51526f6f7b485d576396c81a460b87477598640f
renzhamin <renzhamin@gmail.com>
2023-11-03 14:17:27 +0600

    fetch countries and states

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/index.js"
+import "dotenv/config"
 
 const app = express()
 
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,40 @@
 import express from "express"
+import "dotenv/config"
 
 const router = express.Router()
 
-router.use("/health", (req, res) => {
+const ext_api = "http://api.airvisual.com/v2"
+const api_key = process.env.API_KEY
+
+console.log(api_key)
+
+async function get_countries() {
+    let data = await fetch(ext_api + "/countries?key=" + api_key)
+    data = await data.json()
+    const countries = []
+    data.data.forEach((item) => countries.push(item.country))
+    return countries
+}
+
+async function get_states(country) {
+    let data = await fetch(
+        ext_api +
+            "/states?" +
+            new URLSearchParams({
+                country,
+                key: api_key,
+            })
+    )
+    data = await data.json()
+    const states = []
+    data.data.forEach((item) => states.push(item.state))
+    return states
+}
+
+router.use("/health", async (req, res) => {
+    const states_bd = await get_states("Bangladesh")
+    console.log(states_bd)
+    /* return res.status(200).json({ countries }) */
     return res.sendStatus(200)
 })
 
